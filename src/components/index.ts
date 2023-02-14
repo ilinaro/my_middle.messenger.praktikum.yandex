96ef4bc231e27baefa5340ad65fbd9be6a5f233e
Oleg Ilin <ilindev78@gmal.com>
2023-02-15 02:14:23 +0300

    upd ts -> all file and add devDependencies

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+export { Button } from './buttons/button'
+export { ButtonBack } from './buttons/button-back'
+
+export { Input } from './fields/input'
+export { InputMessage } from './fields/input-message'
+export { InputProfile } from './fields/input-profile'
+export { InputSearch } from './fields/input-search'
+
+export { ModalFileError } from './modals/modal-file-error'
+export { ModalFileOnload } from './modals/modal-file-onload'
+export { ModalFileUpload } from './modals/modal-file-upload'
+export { ModalUserAdd } from './modals/modal-user-add'
+export { ModalUserDelete } from './modals/modal-user-delete'
