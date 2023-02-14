64ddc489ea7a8a0c4b45165639765ca0f9cd2f6b
Oleg Ilin <ilindev78@gmal.com>
2023-02-14 03:06:22 +0300

    upd check list

--- src/components/common/edit-password/index.js
+++ src/components/common/edit-password/index.js
@@ -1,4 +1,4 @@
-import Handlebars from 'handlebars/dist/handlebars.runtime'
 import EditPassword from './edit-password.hbs'
+import Handlebars from 'handlebars/dist/handlebars.runtime'
 
 Handlebars.registerPartial('EditPassword', EditPassword)
