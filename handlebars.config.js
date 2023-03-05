35275647ad9cbfef11a766438bb48b8778b78bd9
Oleg Ilin <ilindev78@gmal.com>
2023-03-05 04:36:10 +0300

    upd for test

--- handlebars.config.js
+++ handlebars.config.js
@@ -1,6 +1,6 @@
 exports = {
-  assets: 'src/assets/*',
-  components: 'src/components/*',
-  blocks: 'src/blocks/*',
-  layouts: 'src/layouts/*',
+  assets: 'src/assets',
+  components: 'src/components',
+  blocks: 'src/blocks',
+  layouts: 'src/layouts',
 };
